f865f28c9f1e1aef00c9de583430e12dd9bd037b
Abishek0708 <abiroy8162@gmail.com>
2024-06-20 21:20:33 +0530

    updated

---
+++ text2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class hi extends StatefulWidget {
+  const hi({super.key});
+
+  @override
+  State<hi> createState() => _hiState();
+}
+
+class _hiState extends State<hi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
